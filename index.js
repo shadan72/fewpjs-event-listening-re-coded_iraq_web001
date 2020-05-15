44a07566b862166328593a3f0169c8e07c846903
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-15 11:09:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addingEventListener() {
   const input=document.getElementById('input')
-  input.addingEventListener('click',
+  input.addEventListener('click',
   function(event){
     alert('I was clicked')
   })
